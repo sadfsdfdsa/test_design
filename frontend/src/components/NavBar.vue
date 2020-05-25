b50fc6131bfbed42c39d1619ff3d8122f1c53c15
sadfsdfdsa <shuvaev13@mail.ru>
2020-05-25 14:26:42 +0500

    sdf

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -4,25 +4,24 @@
             <b-col sm="12" lg="8">
                 <b-navbar toggleable="lg">
                     <b-row class="navbar-row" align-v="center">
-                        <b-col class="text-left" lg="6" md="10">
-                            <b-row align-v="center">
-                                <b-col class="text-left" lg="12" cols="10">
-                                    <div style="display: inline-block">
-                                        <router-link to="/" tag="div" class="cursor h-font text-uppercase">
-                                            <span class="brand-animation-pink">On</span>|<span
-                                                class="brand-animation-blue">Testing</span></router-link>
-                                    </div>
-                                    <!--                                        <span class="badge badge-warning" style="vertical-align: top">Beta</span>-->
-                                </b-col>
-                                <b-col class="text-right" cols="2">
-                                    <b-navbar-toggle target="nav-text-collapse"
-                                                     class="d-lg-none text-right border-0 bg-white">
+                        <b-col class="text-left" lg="4" cols="12">
+                            <b-row class="text-nowrap">
+                                <b-col cols="2">
+                                    <b-navbar-toggle
+                                            target="nav-text-collapse"
+                                            class="text-right border-0 bg-transparent non-focus">
                                         <img src="../../../../lectures_hse/frontend/src/assets/Hamburger_icon.svg">
                                     </b-navbar-toggle>
                                 </b-col>
+                                <b-col cols="10" class="text-nowrap">
+                                    <router-link to="/" tag="div" class="cursor h-font text-uppercase">
+                                        <span class="brand-animation-pink">On</span>|<span
+                                            class="brand-animation-blue">Testing</span>
+                                    </router-link>
+                                </b-col>
                             </b-row>
                         </b-col>
-                        <b-col class="text-uppercase" lg="4">
+                        <b-col class="text-uppercase" lg="5" cols="0">
                             <b-collapse id="nav-text-collapse" is-nav>
                                 <div class="mr-4">
                                     <a href="https://hse-onlecture.herokuapp.com/"><span
@@ -38,7 +37,8 @@
                                 <div class="mr-4">
                                     <router-link to="/test" tag="div" class="navbar-item">Test</router-link>
                                 </div>
-                                <div class="mr-4 navbar-item-font" style="border-right: 2px solid #333;">
+                                <div class="mr-4 navbar-item-font d-none d-lg-block"
+                                     style="border-right: 2px solid #333;">
                                     &#8203;
                                 </div>
                                 <div class="mr-4" v-if="!this.user.login">
